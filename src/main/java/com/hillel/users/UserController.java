70a5294558d0615bd50920d3384407f2024170b7
Kolodzheieva <os-mari.kolodzheieva@ODS01L1182.local>
2022-01-29 12:09:50 +0200

    Initial commit

---
+++ src/main/java/com/hillel/users/UserController.java
@@ -0,0 +1,13 @@
+package com.hillel.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
